a991b1482dc4f7fdfadc9b9aa906b8243d9d3b64
quelcev <quelcev@gmail.com>
2024-05-03 02:04:17 +0800

    transfer equalHeight fn from SingleTour to Tours component

--- tours/src/Tour.jsx
+++ tours/src/Tour.jsx
@@ -1,46 +1,18 @@
-import { useEffect, useState } from "react";
-import { getBiggestHeight, throttle } from "./utils";
-
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
   minimumFractionDigits: 0,
 });
 
-const Tour = ({ tour, removeTour }) => {
+const Tour = ({
+  tour,
+  removeTour,
+  descTextMinHeight,
+  titleMinHeight,
+  toggleText,
+  showFullText,
+}) => {
   const { id, image, info, name, price } = tour;
-  const [showFullText, setShowFullText] = useState(false);
-  const [descTextMinHeight, setDescTextMinHeight] = useState(0);
-  const [titleMinHeight, setTitleMinHeight] = useState(0);
-
-  useEffect(() => {
-    const descTextEls = document.querySelectorAll(".info > p");
-    const titleEls = document.querySelectorAll(".info > h5");
-
-    const equalHeight = () => {
-      // reset height to get new biggest height
-      setDescTextMinHeight(0);
-      setTitleMinHeight(0);
-      // set new biggest height
-      setTimeout(() => {
-        if (window.matchMedia("(max-width: 600px)").matches) {
-          setDescTextMinHeight(0);
-          setTitleMinHeight(0);
-        } else {
-          setDescTextMinHeight(getBiggestHeight(descTextEls));
-          setTitleMinHeight(getBiggestHeight(titleEls));
-        }
-      }, 500);
-    };
-    equalHeight();
-    const throttleEqualHeight = throttle(equalHeight, 500);
-    window.addEventListener("resize", throttleEqualHeight);
-    return () => {
-      window.removeEventListener("resize", throttleEqualHeight);
-    };
-  }, [showFullText]);
-
-  const toggleText = () => setShowFullText(!showFullText);
 
   return (
     <article className="single-tour">
@@ -51,17 +23,19 @@ const Tour = ({ tour, removeTour }) => {
       <div className="info">
         <h5 style={{ minHeight: `${titleMinHeight}px` }}>{name}</h5>
         <p
-          style={{ minHeight: showFullText ? `${descTextMinHeight}px` : 0 }}
-          className={showFullText ? "" : "limit"}
+          style={{
+            minHeight: showFullText.includes(id) ? `${descTextMinHeight}px` : 0,
+          }}
+          className={showFullText.includes(id) ? "" : "limit"}
         >
           {info}
         </p>
-        {showFullText ? (
-          <button className="show-less-btn" onClick={toggleText}>
+        {showFullText.includes(id) ? (
+          <button className="show-less-btn" onClick={() => toggleText(id)}>
             Show Less
           </button>
         ) : (
-          <button className="read-more-btn" onClick={toggleText}>
+          <button className="read-more-btn" onClick={() => toggleText(id)}>
             Read More
           </button>
         )}
--- tours/src/Tours.jsx
+++ tours/src/Tours.jsx
@@ -1,10 +1,63 @@
+import { useEffect, useState } from "react";
 import Tour from "./Tour";
+import { getBiggestHeight, throttle } from "./utils";
 
 const Tours = ({ tours, removeTour }) => {
+  const [descTextMinHeight, setDescTextMinHeight] = useState(0);
+  const [titleMinHeight, setTitleMinHeight] = useState(0);
+  const [showFullText, setShowFullText] = useState([]);
+
+  const toggleText = (newId) => {
+    let newShowFullText = [];
+    if (showFullText.includes(newId)) {
+      newShowFullText = [...showFullText].filter((id) => id !== newId);
+    } else {
+      newShowFullText = [...showFullText, newId];
+    }
+    setShowFullText(newShowFullText);
+  };
+
+  useEffect(() => {
+    const descTextEls = document.querySelectorAll(".info > p");
+    const titleEls = document.querySelectorAll(".info > h5");
+
+    const equalHeight = () => {
+      // reset height to get new biggest height
+      setDescTextMinHeight(0);
+      setTitleMinHeight(0);
+      // set new biggest height
+      setTimeout(() => {
+        if (window.matchMedia("(max-width: 600px)").matches) {
+          setDescTextMinHeight(0);
+          setTitleMinHeight(0);
+        } else {
+          setDescTextMinHeight(getBiggestHeight(descTextEls));
+          setTitleMinHeight(getBiggestHeight(titleEls));
+        }
+      }, 500);
+    };
+    equalHeight();
+    const throttleEqualHeight = throttle(equalHeight, 500);
+    window.addEventListener("resize", throttleEqualHeight);
+    return () => {
+      window.removeEventListener("resize", throttleEqualHeight);
+    };
+  }, [showFullText]);
+
   return (
     <div className="tours-container">
       {tours.map((tour) => {
-        return <Tour key={tour.id} tour={tour} removeTour={removeTour} />;
+        return (
+          <Tour
+            key={tour.id}
+            tour={tour}
+            removeTour={removeTour}
+            descTextMinHeight={descTextMinHeight}
+            titleMinHeight={titleMinHeight}
+            toggleText={toggleText}
+            showFullText={showFullText}
+          />
+        );
       })}
     </div>
   );
