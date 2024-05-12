6b7941f53f5f8304602191ed9cde11d1c2229a6b
quelcev <quelcev@gmail.com>
2024-05-12 10:21:08 +0800

    continue (0:00) - https://www.udemy.com/course/react-tutorial-and-projects-course/learn/lecture/31545310#questions/19754768

---
+++ cart-redux-toolkit-tutorial/src/features/cart/cartSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  amount: 0,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+});
+
+const cartReducer = cartSlice.reducer;
+
+export default cartReducer;
