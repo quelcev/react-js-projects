2dce087d450e063dd68e03b191aca8d1c90302fe
quelcev <quelcev@gmail.com>
2024-05-14 14:35:50 +0800

    continue (2:27) - https://www.udemy.com/course/react-tutorial-and-projects-course/learn/lecture/31545358#questions/19754768

--- cart-redux-toolkit-tutorial/src/components/CartItem.js
+++ cart-redux-toolkit-tutorial/src/components/CartItem.js
@@ -1,21 +1,36 @@
+import { removeCartItem, toggleAmount } from "../features/cart/cartSlice";
 import { ChevronDown, ChevronUp } from "../icons";
+import { useDispatch } from "react-redux";
 
 const CartItem = ({ item }) => {
   const { id, img, title, price, amount } = item;
+  const dispatch = useDispatch();
+
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">${price}</h4>
-        <button className="remove-btn">remove</button>
+        <button
+          className="remove-btn"
+          onClick={() => dispatch(removeCartItem({ id }))}
+        >
+          remove
+        </button>
       </div>
       <div>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => dispatch(toggleAmount({ type: "increase", id }))}
+        >
           <ChevronUp />
         </button>
         <p className="amount">{amount}</p>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => dispatch(toggleAmount({ type: "decrease", id }))}
+        >
           <ChevronDown />
         </button>
       </div>
--- cart-redux-toolkit-tutorial/src/features/cart/cartSlice.js
+++ cart-redux-toolkit-tutorial/src/features/cart/cartSlice.js
@@ -15,10 +15,24 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeCartItem: (state, { payload: { id } }) => {
+      state.cartItems = state.cartItems.filter((item) => item.id !== id);
+    },
+    toggleAmount: (state, { payload: { type, id } }) => {
+      const itemIndex = state.cartItems.findIndex((item) => item.id === id);
+      if (type === "increase") {
+        state.cartItems[itemIndex].amount += 1;
+      } else {
+        state.cartItems[itemIndex].amount -= 1;
+        if (state.cartItems[itemIndex].amount === 0) {
+          state.cartItems.splice(itemIndex, 1);
+        }
+      }
+    },
   },
 });
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeCartItem, toggleAmount } = cartSlice.actions;
 
 const cartReducer = cartSlice.reducer;
 export default cartReducer;
